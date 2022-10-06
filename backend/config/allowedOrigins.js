801504cf2f4d3c66fff6be2295d4b7eaa45fb5e9
adelpro <47066151+adelpro@users.noreply.github.com>
2022-10-06 18:36:18 +0100

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3500", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3500", "http://localhost:3000","https://tvf55m.csb.app"];
 
 module.exports = allowedOrigins;

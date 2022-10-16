8cbc39a58579bd3eb3aa85bb2c7c67358b87ef31
adelpro <47066151+adelpro@users.noreply.github.com>
2022-10-16 16:15:26 +0100

    minor fix

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http:127.0.0.1:3000",
-  "https://mern-auth-roles.onrender.com/",
+  "https://mern-auth-roles.onrender.com",  
 ];
 module.exports = allowedOrigins;

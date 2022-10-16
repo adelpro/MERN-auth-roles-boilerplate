f6bb4f3dcc5606aad207726215ee984d90df05bb
adelpro <adelpro@gmail.com>
2022-10-16 12:11:21 +0100

    error fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import { disabledevtools } from "@fvilers/disable-react-devtools";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 if (process.env.NODE_ENV === "production") {
-  disabledevtools();
+  disableReactDevTools();
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

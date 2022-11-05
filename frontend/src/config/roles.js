44ce2fbe0912c928ae3c71449aa36fd315cc7c2c
adelpro <adelpro@gmail.com>
2022-11-05 18:21:16 +0000

    fix

--- frontend/src/config/roles.js
+++ frontend/src/config/roles.js
@@ -1,10 +1,7 @@
-//export const ROLES = {
-// Employee: "Employee",
-// Manager: "Manager",
-// Admin: "Admin",
-//};
-export const ROLES = [
+const ROLES = [
     { label: 'Employee', value: 'Employee' },
     { label: 'Manager', value: 'Manager' },
     { label: 'Admin', value: 'Admin' },
 ]
+
+export default ROLES

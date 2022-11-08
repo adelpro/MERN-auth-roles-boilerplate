106d0b9c7b61273c18c3672c502e1e2d3ec7827a
adelpro <adelpro@gmail.com>
2022-11-08 13:18:00 +0000

    useAuth fix

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,35 +1,28 @@
 import { useRecoilValue } from 'recoil'
 import { AccessToken } from '../recoil/atom'
 import jwtDecode from 'jwt-decode'
-import { useEffect, useState } from 'react'
 const useAuth = () => {
   const accessToken = useRecoilValue(AccessToken)
-  const [isAdmin, setIsAdmin] = useState(false)
-  const [isManager, setIsManager] = useState(false)
-  const [status, setStatus] = useState('Employee')
-  const [userInfo, setUserInfo] = useState({
+  let isAdmin = false
+  let isManager = false
+  let status = 'Employee'
+
+  if (accessToken) {
+    const decode = jwtDecode(accessToken)
+    const { username, id, roles } = decode.UserInfo
+    isAdmin = roles.includes('Admin')
+    isManager = roles.includes('Manager')
+    if (isManager) status = 'Manager'
+    if (isAdmin) status = 'Admin'
+    return { username, roles, id, status, isAdmin, isManager }
+  }
+  return {
     username: '',
     id: null,
     roles: [],
-  })
-  useEffect(() => {
-    setIsAdmin(true)
-    if (accessToken) {
-      console.log({ accessToken })
-      const decode = jwtDecode(accessToken)
-      setUserInfo(decode.UserInfo)
-      if (decode.UserInfo.roles.includes('Admin')) {
-        console.log('testvalid')
-        setIsAdmin(true)
-        setStatus('Admin')
-      }
-      if (decode.UserInfo.roles.includes('Manager')) {
-        setIsManager(true)
-        setStatus('Manager')
-      }
-    }
-  }, [accessToken])
-  const { username, roles, id } = userInfo
-  return { username, roles, id, status, isAdmin, isManager }
+    status,
+    isAdmin,
+    isManager,
+  }
 }
 export default useAuth
